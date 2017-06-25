044a55a713a267fe374cac3c231d21b95753c44a
ksomemo <kntsmy@hotmail.co.jp>
2017-06-25 23:31:57 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+__version__ = "0.0.1"
+
+setup(
+    name="kaggle_accept",
+    version=__version__,
+    description="",
+    long_description="",
+    author="ksomemo",
+    url="https://github.com/ksomemo/kaggle-accept",
+    license="MIT",
+    packages=find_packages(),
+    keywords="",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ]
+)
